dab5f5d7a17881dc609964159b3ea9a3634999d8
sumantakarmakar <karmakar.sumanta009@gmail.com>
2016-05-19 00:01:05 +0530

    qd

---
+++ musichub/src/main/java/com/musichub/controller/indexcontroller.java
@@ -0,0 +1,33 @@
+package com.musichub.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class indexcontroller {
+	@RequestMapping(value="/index")
+	public ModelAndView indexview()
+	{
+		return new ModelAndView("index");
+	}
+	@RequestMapping(value="contactus")
+	public ModelAndView contactusview()
+	{
+		return new ModelAndView("contactus");
+	}
+	@RequestMapping(value="login")
+	public ModelAndView loginview()
+	{
+		return new ModelAndView("login");
+	}
+	@RequestMapping(value="signup")
+	public ModelAndView signupview()
+	{
+		return new ModelAndView("signup");
+	}
+	@RequestMapping(value="aboutus")
+	public ModelAndView aboutusview()
+	{
+		return new ModelAndView("aboutus");
+	}
+}
